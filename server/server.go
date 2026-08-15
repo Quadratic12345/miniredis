0b0f25f01b1157cf10a173344257298a13c2d6f8
sankalp-kar <programmercoding01234@gmail.com>
2026-08-15 19:36:24 +0530

    sometihng added

---
+++ server/server.go
@@ -0,0 +1,79 @@
+package main
+import (
+	"bufio";"fmt";"net";"strings"
+)
+type Server struct{
+	store *Store
+}
+func NewServer(store *Store) *Server{
+	return &Server{
+		store: store,
+	}
+}
+func (s *Server) Start() error{
+	ln,err :=net.Listen("tcp",":9999")
+	if err !=nil{
+		return err
+	}
+	defer ln.Close()
+	fmt.Println("it is listening on :9999")
+	for {
+		conn,err:=ln.Accept()
+		if err !=nil{
+			fmt.Println("Accept error:",err)
+			continue
+		}
+		go s.handleConnection(conn)
+	}
+}
+func (s *Server) handleConnection(conn net.Conn){
+	defer conn.Close()
+	fmt.Println("Client is connected",conn.RemoteAddr())
+	scanner:=bufio.NewScanner(conn)
+	for scanner.Scan(){
+		comand:=scanner.Text()
+		response:=s.handleCommand(command)
+		fmt.Fprintln(conn.response)
+	}
+	fmt.Println("Client is disconnected",conn.RemoteAddr())
+}
+func (s *Server) handleComand(comand string) string{
+	parts:=strings.Fields(command)
+	if len(parts)==0{
+		return "error empty command"
+	}
+	switch strings.ToUpper(parts[0]){
+	case "SET":
+		if len(parts)<3{
+			return "error usage set key value"
+		}
+		key :=parts[1]
+		value:=strings.Join(parts[2:],"")
+		s.store.Set(key,value)
+		return "OK"
+	case "GET":
+		if len(parts) !=2{
+			return "error usage get key"
+		}
+		key :=parts[1]
+		value,exists :=s.store.Get(key)
+		if !exists{
+			return "(nil)"
+		}
+		return value
+	case "DEL":
+		if len(parts) !=2{
+			return "error usage del key"
+		}
+		key :=parts[1]
+		deleted :=s.store.Delete(key)
+		if deleted{
+			return "1"
+		}
+		return "0"
+
+	default:
+		return "error command not known"
+
+	}
+}
